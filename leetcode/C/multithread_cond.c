974dcdef944b42af17cd257b5351f009d175b5d3
yanggogo <x18127891567@163.com>
2021-02-05 23:45:42 +0800

    更新对条件变量的理解

--- leetcode/C/multithread_cond.c
+++ leetcode/C/multithread_cond.c
@@ -28,8 +28,13 @@ void foo(FooBar* obj) {
         
         // printFoo() outputs "foo". Do not change or remove this line.
         pthread_mutex_lock(&obj->lock);
+
+        // 等待的条件为false，调用以下语句后
         while (!obj->flag)
         {
+            // 调用pthread_cond_wait有两个效果：
+            // 1. 当前线程阻塞，直到其他线程pthread_cond_signal(&obj->cond, &obj->lock)后，本函数才返回
+            // 2. 释放obj->lock锁 ————> pthread_cond_wait内部会释放锁，当该线程被唤醒，又会自动获取锁
             pthread_cond_wait(&obj->cond, &obj->lock);
         }
         printFoo();
@@ -68,3 +73,8 @@ void fooBarFree(FooBar* obj) {
 // 2. 如果初始化的时候，Bar先获得锁，是不是就死锁了？
 // 3. foo 函数会不会再次抢到锁，导致死循环？
 
+
+// 使用条件变量需要初始化三个变量：
+// 1. 定义一个条件                      ———— 用于业务逻辑
+// 2. 定义一个修改该条件的互斥量（锁）    ———— 用于互斥的修改条件
+// 3. 定义一个条件变量（pthread_cond_t） ———— 用于线程间signal和线程阻塞
